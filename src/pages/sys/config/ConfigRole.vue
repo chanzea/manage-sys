17b4769c44b8841dd3418cce266689a25ec2dfeb
DK <dk.zhang@jodoinc.com>
2019-10-15 00:19:21 +0800

    角色权限设置

--- src/pages/sys/config/ConfigRole.vue
+++ src/pages/sys/config/ConfigRole.vue
@@ -2,7 +2,7 @@
   <div class="page-config-role">
     <div class="config-role-content">
       <div class="content-header">
-        <Input placeholder="输入角色名称" >
+        <Input placeholder="输入角色名称">
           <Button slot="append" icon="md-add">新增角色</Button>
         </Input>
       </div>
@@ -10,9 +10,10 @@
         <Table :columns="columns" :data="data"></Table>
       </div>
       <Modal v-model="isShowModal" fullscreen :title="modalContent.title">
-        <div>{{modalContent.content}}</div>
+        <!-- <div>{{modalContent.content}}</div> -->
         <div>
-          <Tree :data="data" show-checkbox></Tree>
+          <Button slot="append" icon="md-add" @click="getdd">test</Button>
+          <Tree :data="treeData" ref="tree" :render="renderContent" show-checkbox></Tree>
         </div>
       </Modal>
     </div>
@@ -20,100 +21,200 @@
 </template>
 
 <script>
+import { getRoleList, getRoleListPermission } from "api/role.js";
+
 import {
-  getRoleList,
-  getRoleListPermission
-} from 'api/role.js';
+  getListTree
+} from "api/sysConfig";
+
 export default {
-  name: 'ConfigRole',
+  name: "ConfigRole",
   data() {
     return {
       isShowModal: false,
-      columns: [{
-        type: 'selection',
-        width: 60,
-        align: 'center'
-      },{
-        title: '角色',
-        key: 'roleName'
-      },
-      {
-        title: '权限',
-        key: 'permission',
-        width: 400,
-        render: (h, params) => {
-          return h('div', [
-            h('span', {
-              style: {
-                color: '#2d8cf0',
-                marginRight: '12px',
-                cursor: 'pointer'
-              },
-              on: {
-                click: () => {
-                    this.getRoleListPermission(params.row)
-                }
-              }
-            }, '配置权限')
-          ]);
-        }
-      },
-      {
-        title: '操作',
-        key: 'action',
-        width: 200,
-        render: (h, params) => {
-          return h('div', [
-            h('span', {
-              style: {
-                color: '#2d8cf0',
-                marginRight: '12px',
-                cursor: 'pointer'
-              },
-              on: {
-                click: () => {
-                    this.delete(params.row)
-                }
-              }
-            }, '删除')
-          ]);
-        }
-      }],
       data: [],
+      columns: [
+        {
+          type: "selection",
+          width: 60,
+          align: "center"
+        },
+        {
+          title: "角色",
+          key: "roleName"
+        },
+        {
+          title: "权限",
+          key: "permission",
+          width: 400,
+          render: (h, params) => {
+            return h("div", [
+              h(
+                "span",
+                {
+                  style: {
+                    color: "#2d8cf0",
+                    marginRight: "12px",
+                    cursor: "pointer"
+                  },
+                  on: {
+                    click: () => {
+                      this.getRoleListPermission(params.row);
+                    }
+                  }
+                },
+                "配置权限"
+              )
+            ]);
+          }
+        },
+        {
+          title: "操作",
+          key: "action",
+          width: 200,
+          render: (h, params) => {
+            return h("div", [
+              h(
+                "span",
+                {
+                  style: {
+                    color: "#2d8cf0",
+                    marginRight: "12px",
+                    cursor: "pointer"
+                  },
+                  on: {
+                    click: () => {
+                      this.delete(params.row);
+                    }
+                  }
+                },
+                "删除"
+              )
+            ]);
+          }
+        }
+      ],
       modalContent: {
-        title: '',
+        title: "",
         content: null
-      }
-    }
+      },
+      //树形
+      treeData: [
+        {
+          permissionName: "平台",
+          expand: true,
+          children: []
+        }
+      ],
+      buttonProps: {
+        type: "default",
+        size: "small"
+      },
+      dragstartNode: "",
+      dragstartData: ""
+    };
   },
-  created () {
-    this.getRoleList()
+  created() {
+    this.getRoleList();
+    // this.getList();
   },
   methods: {
-    delete (row) {
-      console.log('row', row)
+    getdd(){
+      let checkedNodes = this.$refs.tree.getCheckedNodes();
+      let checkedId = checkedNodes.map( item => item.id);
+      console.log(checkedId);
     },
-    getRoleListPermission (row) {
-      console.log('row', row)
-      this.modalContent.title = row.roleName
-      this.isShowModal = true
-      getRoleListPermission({
-        roleId: row.id
-      }).then(res => {
-        console.log('res', res)
-        const {list} = res
-        this.modalContent.content = list
-      })
+    delete(row) {
+      console.log("row", row);
     },
-    getRoleList () {
+    async getRoleListPermission(row) {
+      console.log("row", row);
+      this.modalContent.title = row.roleName;
+      this.isShowModal = true;
+
+      let dataList = await Promise.all([getListTree(),getRoleListPermission()]);
+
+      let treeList = dataList[0].list;
+
+      treeList.forEach(item => {
+        this.formatTreeData(item);
+      });
+      this.$set(this.treeData[0], "children", treeList);
+      let permissionList = dataList[1].list;
+
+      console.log("permissionList", permissionList);
+      console.log("treeList", "treeList");
+
+    },
+    getRoleList() {
       getRoleList().then(res => {
-        console.log('res', res)
-        const { list } = res
-        this.data = list
-      })
+        console.log("res", res);
+        const { list } = res;
+        this.data = list;
+      });
+    },
+
+    // async getList() {
+    //   let list = getListTree().then(res => {
+    //     console.log(res);
+    //     let list = res.list;
+    //     console.log(list);
+    //     list.forEach(item => {
+    //       this.formatTreeData(item);
+    //     });
+    //     this.$set(this.treeData[0], "children", list);
+    //   });
+
+    //   let permissionList = await Promise.all([getListTree(),getRoleList()]);
+      
+    //   console.log("permissionList" ,permissionList)
+
+    // },
+
+    formatTreeData(item) {
+      if (!item.childs) {
+        item.isEdit = false;
+        item.expand = true;
+        
+        return [item];
+      }
+      item.checked = true;
+      item.children = item.childs;
+      item.expand = !!item.childs;
+      item.childs.forEach(subItem => {
+        this.formatTreeData(subItem);
+      });
+      return [item];
+    },
+
+    renderContent(h, { root, node, data }) {
+      return h(
+        "span",
+        {
+          style: {
+            display: "inline-block",
+            width: "100%"
+          }
+        },
+        [
+          h("span", [
+            h(
+              "span",
+              {
+                style: {
+                  display: "unset"
+                }
+              },
+              `${data.permissionName}${
+                data.permissionUri ? "(" + data.permissionUri + ")" : ""
+              }`
+            )
+          ])
+        ]
+      );
     },
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
