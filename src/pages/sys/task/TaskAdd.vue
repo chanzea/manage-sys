7368f613e4d129ce8448da2eb0791ee6c75367aa
DK <dk.zhang@jodoinc.com>
2019-10-19 18:30:46 +0800

    任务添加

--- src/pages/sys/task/TaskAdd.vue
+++ src/pages/sys/task/TaskAdd.vue
@@ -8,50 +8,55 @@
         <FormItem label="任务名称">
           <Input v-model="formItem.taskName" placeholder="编号"></Input>
         </FormItem>
-         <FormItem label="任务用途">
-          <Input v-model="formItem.use" type="textarea" :autosize="{minRows: 2,maxRows: 5}" placeholder="描述"></Input>
+        <FormItem label="任务用途">
+          <Input
+            v-model="formItem.taskRemark"
+            type="textarea"
+            :autosize="{minRows: 2,maxRows: 5}"
+            placeholder="描述"
+          ></Input>
         </FormItem>
         <FormItem label="标注任务积分">
           <div class="task-mark">
             <span style="margin-right:12px">按</span>
-            <Select class="task-mark-select" v-model="formItem.integral">
+            <Select class="task-mark-select" v-model="formItem.markPointType">
               <Option value="beijing">New York</Option>
               <Option value="shanghai">London</Option>
               <Option value="shenzhen">Sydney</Option>
             </Select>
             <span style="margin:0 12px">积分,每次</span>
-            <Input class="task-mark-input" type="text" placeholder="积分"></Input>
+            <Input class="task-mark-input" type="number" v-model="formItem.markPoint"  placeholder="积分"></Input>
           </div>
         </FormItem>
         <FormItem label="审核任务积分">
           <div class="task-mark">
             <span style="margin-right:12px">完成任务累计</span>
-            <Input class="task-mark-input" type="text" placeholder="积分"></Input>
+            <Input class="task-mark-input" type="text" v-model="formItem.reviewPoint" placeholder="积分"></Input>
           </div>
         </FormItem>
         <FormItem label="任务模版">
-          <Select v-model="formItem.select">
+          <Select v-model="formItem.taskType">
             <Option value="beijing">New York</Option>
             <Option value="shanghai">London</Option>
             <Option value="shenzhen">Sydney</Option>
           </Select>
         </FormItem>
         <FormItem label="数据源">
-          <Select v-model="formItem.select" placeholder="选择数据源">
+          <Select v-model="formItem.dataSetId" placeholder="选择数据源">
             <Option value="beijing">New York</Option>
             <Option value="shanghai">London</Option>
             <Option value="shenzhen">Sydney</Option>
           </Select>
         </FormItem>
         <FormItem label="标注人员">
-          <Select v-model="formItem.select">
+          <Select v-model="formItem.markUserIds" multiple filterable clearable>
             <Option value="beijing">New York</Option>
             <Option value="shanghai">London</Option>
             <Option value="shenzhen">Sydney</Option>
           </Select>
         </FormItem>
         <FormItem label="审核人员">
-          <Select v-model="formItem.select">
+          <Select v-model="formItem.reviewUserIds"  multiple filterable clearable>
             <Option value="beijing">New York</Option>
             <Option value="shanghai">London</Option>
             <Option value="shenzhen">Sydney</Option>
@@ -68,18 +73,18 @@
 
 <script>
 export default {
-  name: 'TaskAdd',
-  data () {
+  name: "TaskAdd",
+  data() {
     return {
-      formItem: {}
-    }
+      formItem: {
+
+      }
+    };
   },
   methods: {
-    getFormData () {
-
-    }
+    getFormData() {}
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -96,7 +101,7 @@ export default {
         width: 85px;
         position: relative;
         &::after {
-          content: '分';
+          content: "分";
           position: absolute;
           top: 7px;
           right: -20px;
