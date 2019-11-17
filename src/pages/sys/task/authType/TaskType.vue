5e091846d81334168b9797aad0d937e90c66fd74
chanzea <876890520@qq.com>
2019-11-17 18:37:10 +0800

    增加查看源文件

--- src/pages/sys/task/authType/TaskType.vue
+++ src/pages/sys/task/authType/TaskType.vue
@@ -9,8 +9,8 @@ import TaskClassify from './TaskClassify'
 import TaskRejectPic from './TaskRejectPic'
 import TaskRejectFolder from './TaskRejectFolder'
 
-import TaskMark from '../tagtoolMain/src/views/ReviewMark'
-import TaskLasso from '../tagtoolMain/src/views/ReviewMark'
+// import TaskMark from '../tagtoolMain/src/views/ReviewMark'
+// import TaskLasso from '../tagtoolMain/src/views/ReviewMark'
 
 export default {
   name: 'TaskType',
